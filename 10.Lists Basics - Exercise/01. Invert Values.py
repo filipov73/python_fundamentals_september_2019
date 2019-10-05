995b80f77b7645066fd852a4514bff7a02d9d83a
MarioFilco <m_filipov@yahoo.com>
2019-10-05 12:04:46 +0300

    2

---
+++ 10.Lists Basics - Exercise/01. Invert Values.py
@@ -0,0 +1,2 @@
+print(list(map(lambda x: int(x) * -1, input().split(" "))))
+
---
+++ 10.Lists Basics - Exercise/02. Multiples List.py
@@ -0,0 +1,6 @@
+# num = int(input())
+# print(lambda x: x, int(input()))
+print(list(lambda num: num, [int(input())]))
+# print(list(map(lambda x: x * list(map(lambda num: num, [int(input())])), range(1, int(input())+1))))
+
+a=5
