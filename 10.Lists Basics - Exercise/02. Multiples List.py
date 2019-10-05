d6d10531ab1718b689befc945dd5492a372fc232
home <m_filipov@yahoo.com>
2019-10-05 13:34:52 +0300

    1

--- 10.Lists Basics - Exercise/02. Multiples List.py
+++ 10.Lists Basics - Exercise/02. Multiples List.py
@@ -1,6 +1,2 @@
-# num = int(input())
-# print(lambda x: x, int(input()))
-print(list(lambda num: num, [int(input())]))
-# print(list(map(lambda x: x * list(map(lambda num: num, [int(input())])), range(1, int(input())+1))))
+print(*map(lambda num: list(map(lambda x: x * num, range(1, int(input())+1))), [int(input())]))
 
-a=5
