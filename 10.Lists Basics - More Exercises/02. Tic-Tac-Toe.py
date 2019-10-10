c91bd880a2a54ac3a8875ecbab2b6efb63c0a438
MarioFilco <m_filipov@yahoo.com>
2019-10-10 23:52:04 +0300

    2

--- 10.Lists Basics - More Exercises/02. Tic-Tac-Toe.py
+++ 10.Lists Basics - More Exercises/02. Tic-Tac-Toe.py
@@ -1,13 +1,31 @@
-import numpy
+import numpy as np
 
-line_1 = input().split(" ")
-line_2 = input().split(" ")
-line_3 = input().split(" ")
+line_1 = list(map(int, input().split(" ")))
+line_2 = list(map(int, input().split(" ")))
+line_3 = list(map(int, input().split(" ")))
 
-matrix = [
+matrix = np.array([
     line_1,
     line_2,
     line_3
-]
+])
+winner = None
+for i in range(3):
+    if len(set(matrix[:, i])) == 1:
+        winner = list(set(matrix[:, i]))[0]
+        break
+    elif len(set(matrix[i, :])) == 1:
+        winner = list(set(matrix[i, :]))[0]
+        break
 
-print(*map(int, matrix))
+if len(set(matrix.diagonal())) == 1:
+    winner = list(set(matrix.diagonal()))[0]
+elif len(set(np.fliplr(matrix).diagonal())) == 1:
+    winner = list(set(np.fliplr(matrix).diagonal()))[0]
+if winner is not None:
+    if winner == 1:
+        print("First player won")
+    else:
+        print("Second player won")
+
+
