6894fdfd36741932bb612cb68e03d7e3a0bbaea4
MarioFilco <m_filipov@yahoo.com>
2019-10-17 23:23:16 +0300

    2

--- 16.Lists Advanced - Exercise/01. Which Are In.py
+++ 16.Lists Advanced - Exercise/01. Which Are In.py
@@ -1,10 +1,16 @@
+# first_list = input().split(", ")
+# second_list = input().split(", ")
+#
+# res_list = []
+#
+# for st_1 in first_list:
+#     for st_2 in second_list:
+#         if st_1 in st_2:
+#             res_list.append(st_1)
+# print(sorted(set(res_list), key=res_list.index))
+
+
 first_list = input().split(", ")
 second_list = input().split(", ")
-
-res_list = []
-
-for st_1 in first_list:
-    for st_2 in second_list:
-        if st_1 in st_2:
-            res_list.append(st_1)
-print(sorted(set(res_list), key=res_list.index))
+res = [first for first in first_list for second in second_list if first in second]
+print(sorted(set(res), key=res.index))
