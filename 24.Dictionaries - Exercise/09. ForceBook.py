cd7203ed921a22899d4c69744985216467af9116
MarioFilco <m_filipov@yahoo.com>
2019-11-13 01:21:05 +0200

    2

--- 24.Dictionaries - Exercise/09. ForceBook.py
+++ 24.Dictionaries - Exercise/09. ForceBook.py
@@ -13,15 +13,19 @@ while command != "Lumpawaroo":
         user, side = command.split(" -> ")
         if side not in force_book:
             force_book[side] = []
-        # if user not in list(x for y in force_book.values() for x in y):
-        if user not in force_book[side]:
+        a = list(x for y in force_book.values() for x in y)
+        if user not in list(x for y in force_book.values() for x in y):
             force_book[side].append(user)
-            print(f"{user} joins the {side} side!" )
+            print(f"{user} joins the {side} side!")
         else:
-            remove_user_side = [x for x in force_book.keys() if x != side][0]
-            change_user_side = force_book[remove_user_side].pop(force_book[remove_user_side].index(user))
-            force_book[side].append(user)
-            print(f"{user} joins the {side} side!" )
+            for (key, value) in force_book.items():
+                if user in value:
+                    force_book[key].remove(user)
+                    force_book[side].append(user)
+            # remove_user_side = [key for (key, value) in force_book.items() if user in value][0]
+            # change_user_side = force_book[remove_user_side].pop(force_book[remove_user_side].index(user))
+            # force_book[side].append(user)
+            print(f"{user} joins the {side} side!")
 
     command = input()
 
