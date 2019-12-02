f4dcc5db220d8869bbbd0e1ed83ac9a4c2363327
MarioFilco <m_filipov@yahoo.com>
2019-12-03 00:46:24 +0200

    2

--- Exam/Programming Fundamentals Final Exam Retake - 9 August 2019/2.Password.py
+++ Exam/Programming Fundamentals Final Exam Retake - 9 August 2019/2.Password.py
@@ -1,12 +1,13 @@
 import re
 number = int(input())
 
-pattern = r"^(?P<start>.+)\>(?P<first>\d{3})[|](?P<second>[a-z]{3})[|](?P<third>[A-Z]{3})[|](?P<fourth>[^>|<]{3})\<(?P<end>.+)$"
+# pattern = r"^(?P<start>.+)\>(?P<first>\d{3})[|](?P<second>[a-z]{3})[|](?P<third>[A-Z]{3})[|](?P<fourth>[^>|<]{3})\<(?P<end>.+)$"
+pattern = r"^(?P<start>.+)\>(?P<first>\d{3})[|](?P<second>[a-z]{3})[|](?P<third>[A-Z]{3})[|](?P<fourth>[^>|<]{3})\<(?P=start)$"
 
 for _ in range(number):
     password = input()
     target = re.match(pattern, password)
-    if target and target.group("start") == target.group("end"):
+    if target:
         res = target.group("first") + target.group("second") + target.group("third") + target.group("fourth")
         print(f"Password: {res}")
     else:
